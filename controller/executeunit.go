f7ca189c2db8baf5701f71385694ea9d4d3d8e2e
sartmorgs <morgana.sartor@outlook.com>
2021-03-29 02:24:09 -0300

    Add execute unit into controller

--- controller/executeunit.go
+++ controller/executeunit.go
@@ -3,13 +3,67 @@ package controller
 import "strconv"
 
 type ExecuteUnit struct{
-	config map[string]int
+	config map[string]int64
+}
+
+func NewExecuteUnit() *ExecuteUnit{
+	executeunit := new(ExecuteUnit)
+	executeunit.config = make(map[string]int64)
+
+	return executeunit
 }
 
 func (eu *ExecuteUnit) ConfigForLdi(reg string, value string){
-	x, _ := strconv.Atoi(reg)
+	x, _ := strconv.ParseInt(reg, 2, 64)
 	eu.config["register"] = x
 
-	y, _ := strconv.Atoi(value)
+	y, _ := strconv.ParseInt(value, 2, 64)
 	eu.config["value"] = y
+}
+
+func (eu *ExecuteUnit) ConfigForLdr(reg_rt string, reg_rn string){
+	x, _ := strconv.ParseInt(reg_rt, 2, 64)
+	eu.config["register_rt"] = x
+
+	y, _ := strconv.ParseInt(reg_rn, 2, 64)
+	eu.config["register_rn"] = y
+}
+
+func (eu *ExecuteUnit) ConfigForStr(reg_rt string, reg_rn string, ram_addr string){
+	x, _ := strconv.ParseInt(reg_rt, 2, 64)
+	eu.config["register_rt"] = x
+
+	y, _ := strconv.ParseInt(reg_rn, 2, 64)
+	eu.config["register_rn"] = y
+
+	z, _ := strconv.ParseInt(ram_addr, 2, 64)
+	eu.config["ram_address"] = z
+}
+
+func (eu *ExecuteUnit) ConfigForAdds(reg_rd string, reg_rn string, reg_rm string, imm string){
+	x, _ := strconv.ParseInt(reg_rd, 2, 64)
+	eu.config["register_rd"] = x
+
+	y, _ := strconv.ParseInt(reg_rn, 2, 64)
+	eu.config["register_rn"] = y
+
+	z, _ := strconv.ParseInt(reg_rm, 2, 64)
+	eu.config["register_rm"] = z
+
+	m, _ := strconv.ParseInt(imm, 2, 64)
+	eu.config["immediate"] = m
+}
+
+func (eu *ExecuteUnit) ConfigForSubs(reg_rd string, reg_rn string, reg_rm string, imm string){
+	x, _ := strconv.ParseInt(reg_rd, 2, 64)
+	eu.config["register_rd"] = x
+
+	y, _ := strconv.ParseInt(reg_rn, 2, 64)
+	eu.config["register_rn"] = y
+
+	z, _ := strconv.ParseInt(reg_rm, 2, 64)
+	eu.config["register_rm"] = z
+
+	m, _ := strconv.ParseInt(imm, 2, 64)
+	eu.config["immediate"] = m
 }
--- controller/executeunit_test.go
+++ controller/executeunit_test.go
@@ -8,14 +8,81 @@ import (
 var exun ExecuteUnit
 
 func TestConfigForLdi(t *testing.T){
-	want := map[string]int{
+	exun := NewExecuteUnit()
+
+	want := map[string]int64{
 		"register": 2,
 		"value": 70,
 	}
 	exun.ConfigForLdi("00010", "00000001000110")
 	got := exun.config
 
-	if reflect.DeepEqual(want, got){
+	if !reflect.DeepEqual(want, got){
 		t.Errorf("ConfigForLdi \n got: %v \n want %v \n", got, want)
+	}
+}
+
+func TestConfigForLdr(t *testing.T){
+	exun := NewExecuteUnit()
+
+	want := map[string]int64{
+		"register_rt": 2,
+		"register_rn": 4, 
+	}
+	exun.ConfigForLdr("00010", "00100")
+	got := exun.config
+
+	if !reflect.DeepEqual(want, got){
+		t.Errorf("ConfigForLdr \n got: %v \n want %v \n", got, want)
+	}
+}
+
+func TestConfigForStr(t *testing.T){
+	exun := NewExecuteUnit()
+
+	want := map[string]int64{
+		"register_rt": 2,
+		"register_rn": 4,
+		"ram_address": 100, 
+	}
+	exun.ConfigForStr("00010", "00100", "1100100")
+	got := exun.config
+
+	if !reflect.DeepEqual(want, got){
+		t.Errorf("ConfigForStr \n got: %v \n want %v \n", got, want)
+	}
+}
+
+func TestConfigForAdds(t *testing.T){
+	exun := NewExecuteUnit()
+
+	want := map[string]int64{
+		"register_rd": 2,
+		"register_rn": 2,
+		"register_rm": 4,
+		"immediate": 0, 
+	}
+	exun.ConfigForAdds("00010", "00010", "00100", "0")
+	got := exun.config
+
+	if !reflect.DeepEqual(want, got){
+		t.Errorf("ConfigForAdds \n got: %v \n want %v \n", got, want)
+	}
+}
+
+func TestConfigForSubs(t *testing.T){
+	exun := NewExecuteUnit()
+
+	want := map[string]int64{
+		"register_rd": 2,
+		"register_rn": 2,
+		"register_rm": 4,
+		"immediate": 0, 
+	}
+	exun.ConfigForSubs("00010", "00010", "00100", "0")
+	got := exun.config
+
+	if !reflect.DeepEqual(want, got){
+		t.Errorf("ConfigForSubs \n got: %v \n want %v \n", got, want)
 	}
 }
