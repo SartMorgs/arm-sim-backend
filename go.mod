dff09e1d454374c101094d22812c318765df5445
sartmorgs <morgana.sartor@outlook.com>
2021-08-05 00:16:34 -0300

    start api development

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module arm
 
 go 1.15
 
-require github.com/nsf/jsondiff v0.0.0-20210303162244-6ea32392771e
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/nsf/jsondiff v0.0.0-20210303162244-6ea32392771e
+)
--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
-func main(){
+import (
+	"fmt"
+	"log"
+	//"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"github.com/gorilla/mux"
+)
 
+func executeCode(w http.ResponseWriter, r *http.Request){
+	// get the body of our POST request
+	// return the string response
+	reqBody, _ := ioutil.ReadAll(r.Body)
+
+	fmt.Fprintf(w, "%+v", string(reqBody))
+	//fmt.Println("Endpoint Hit: executeCode")
+}
+
+func homePage(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests(){
+	// creates a new instance of a mux router
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/execute", executeCode).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":7000", myRouter))
+}
+
+func main(){
+	fmt.Println("Rest API v2.0 - Mux Routers")
+	handleRequests()
 }
